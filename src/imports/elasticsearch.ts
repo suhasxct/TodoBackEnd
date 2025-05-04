868776338edd11bd17cc561b53c3039c74debe89
Shashi Suhas <firstmage111@gmail.com>
2025-05-05 02:32:07 +0530

    more updates

--- src/imports/elasticsearch.ts
+++ src/imports/elasticsearch.ts
@@ -1,6 +1,6 @@
 import { Client } from "@elastic/elasticsearch";
 export const clientElasticsearch = new Client({
-  node: `${process.env.ELASTIC_URL}:${process.env.ELASTIC_PORT}`,
+  node: `${process.env.ELASTIC_URL}`,
   auth: {
     username: process.env.ELASTIC_USERNAME ?? "",
     password: process.env.ELASTIC_PASSWORD ?? "",
