2636217ee568f71f0b0ce6473c66d4ecd0ef7741
Shashi Suhas <firstmage111@gmail.com>
2025-05-05 00:44:14 +0530

    more updates

--- src/imports/redis.ts
+++ src/imports/redis.ts
@@ -4,15 +4,4 @@ export const clientRedis = createClient({
   url: process.env.REDIS_URL,
 });
 
-const connectToRedis = async () => {
-  if (!clientRedis.isOpen) {
-    try {
-      await clientRedis.connect();
-      console.log("Connected to Redis successfully");
-    } catch (error) {
-      console.error("Error connecting to Redis:", error);
-    }
-  }
-};
-
-connectToRedis();
+clientRedis.connect();
